369b14b3c7820baaa950d9dc15d06828248d61eb
dalinkstone <95542331+dalinkstone@users.noreply.github.com>
2022-12-26 09:02:36 -0700

    Created the populate_db command

--- diablo3citadel/diablo3citadel/mysite/diablo3citadel/management/commands/populate_db.py
+++ diablo3citadel/diablo3citadel/mysite/diablo3citadel/management/commands/populate_db.py
@@ -6,9 +6,22 @@ class Command(BaseCommand):
     args = ''
     help = 'This command populates the PostgreSQL database with the ItemTypeIndex values'
 
-    def _get_values(self):
+    def _create_rows(self):
         values = main_flow()
         id_counter = 0
         name_counter = 1
         path_counter = 2
+
+        while path_counter < len(values):
+            input_row = ItemTypeIndex(id=values[id_counter], name=values[name_counter], path=values[path_counter])
+            input_row.save()
+            id_counter += 3
+            name_counter += 3
+            path_counter += 3
+
+    def handle(self, *args, **options):
+        self._create_rows()
+
+        self.stdout.write(self.style.SUCCESS('Successfully populated the database with the rows.'))
+
         
