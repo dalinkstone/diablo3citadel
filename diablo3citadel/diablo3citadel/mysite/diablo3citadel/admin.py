9cb5b2617c81115877377c152167a9b0c4624b68
dalinkstone <95542331+dalinkstone@users.noreply.github.com>
2022-12-28 18:16:16 -0700

    Added the ItemType to the admin site

--- diablo3citadel/diablo3citadel/mysite/diablo3citadel/admin.py
+++ diablo3citadel/diablo3citadel/mysite/diablo3citadel/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import ItemTypeIndex
+from .models import ItemTypeIndex, ItemType
 
-class ItemAdmin(admin.ModelAdmin):
+class ItemTypeIndexAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,          {'fields': ['id']}),
         ('Item Name',   {'fields': ['name']}),
         ('Item Path',   {'fields': ['path']}),
     ]
+admin.site.register(ItemTypeIndex, ItemTypeIndexAdmin)
 
-
-admin.site.register(ItemTypeIndex, ItemAdmin)
+class ItemTypeAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,              {'fields': ['id']}),
+        ('Item Slug',       {'fields': ['slug']}),
+        ('Item Name',       {'fields': ['name']}),
+        ('Item Icon',       {'fields': ['icon']}),
+        ('Item Path',       {'fields': ['path']}),
+    ]
+admin.site.register(ItemType, ItemTypeAdmin)
